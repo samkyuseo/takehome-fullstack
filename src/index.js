cecd6492539175a5ec1665f1b886e90473578dc5
Samuel Lee <sam.lee@trustalchemy.com>
2021-04-24 17:14:14 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const Routes = require('./routes');
 
-const port = process.env.PORT || 3000;
 const app = express();
+
+// look for environment variable called port when deployed. if local take 3000
+const PORT = process.env.PORT || 3000; 
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
-
+// test 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../README.md')));
 
 app.get('/api/quizzes', Routes.getQuizzes);
 app.get('/api/quizzes/:id', Routes.getQuiz);
 app.post('/api/quizzes/:id/attempt', Routes.postQuiz);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
